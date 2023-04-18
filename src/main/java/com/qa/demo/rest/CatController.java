b7bd10bb829dbb50164108a08a6dd6f109f551b8
Helen Pickavance <helenpjobs@gmail.com>
2023-04-18 15:58:56 +0100

    first commit

---
+++ src/main/java/com/qa/demo/rest/CatController.java
@@ -0,0 +1,13 @@
+package com.qa.demo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CatController {
+    
+    @GetMapping("/") 
+    public String greeting() {
+        return "Hello, World!";
+    }
+}
