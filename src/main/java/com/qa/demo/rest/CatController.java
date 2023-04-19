7f394ddc7fd3e81736db0efd5498e50214aea570
Helen Pickavance <helenpjobs@gmail.com>
2023-04-19 10:28:15 +0100

    CatControlelr delete method

--- src/main/java/com/qa/demo/rest/CatController.java
+++ src/main/java/com/qa/demo/rest/CatController.java
@@ -4,6 +4,7 @@ import com.qa.demo.domain.Cat;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,11 @@ public class CatController {
 
         this.cats.add(newCat);
         return this.cats.get(cats.size() - 1);
+    }
+
+    @DeleteMapping("/remove/{id}")
+    public Cat delete(@PathVariable int id) {
+        return this.cats.remove(id);
     }
 
     @GetMapping("/get/{id}")
