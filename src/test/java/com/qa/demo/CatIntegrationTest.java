7c3bc06c0b5bc9a913c491dc02ee891de10ac973
Helen Pickavance <helenpjobs@gmail.com>
2023-04-20 12:35:02 +0100

    All tests complete and working

--- src/test/java/com/qa/demo/CatIntegrationTest.java
+++ src/test/java/com/qa/demo/CatIntegrationTest.java
@@ -1,5 +1,8 @@
 package com.qa.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -44,7 +47,70 @@ public class CatIntegrationTest {
         String createdAsJson = this.mapper.writeValueAsString(created);
         ResultMatcher checkBody = MockMvcResultMatchers.content().json(createdAsJson);
         
-        // Send request
+        // Send request - expect 201 status and the created cat with ID
         this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+    }
+
+    @Test
+    void testGetByID() throws Exception{
+        // URL, method (GET), body JSON
+        RequestBuilder req = MockMvcRequestBuilders.get("/get/1");
+        Cat returned = new Cat(1L, "Salem", false, true, 10);
+        String returnedAsJson = this.mapper.writeValueAsString(returned);
+        ResultMatcher checkBody = MockMvcResultMatchers.content().json(returnedAsJson);
+        ResultMatcher checkStatus = MockMvcResultMatchers.status().isOk();
+
+        // Send request - and expect 200 status code and the cat (ID 1) from the DB schema
+        this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+    }
+
+    @Test
+    void testGetAll() throws Exception {
+        // URL, method (GET), body JSON
+        RequestBuilder req = MockMvcRequestBuilders.get("/getAll");
+
+        Cat arrayCat = new Cat(1L, "Salem", false, true, 10);
+        List<Cat> cats = new ArrayList<Cat>();
+        cats.add(arrayCat);
+
+        String returnedAsJson = this.mapper.writeValueAsString(cats);
+        ResultMatcher checkBody = MockMvcResultMatchers.content().json(returnedAsJson);
+        ResultMatcher checkStatus = MockMvcResultMatchers.status().isOk();
+
+        // Send request - and expect 200 status code and the cat (ID 1) from the DB schema
+        this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+    }
+
+    @Test
+    void testDelete() throws Exception {
+        // URL, method (GET), body JSON
+        RequestBuilder req = MockMvcRequestBuilders.delete("/remove/1");
+
+        Cat removedCat = new Cat(1L, "Salem", false, true, 10);
+        String returnedAsJson = this.mapper.writeValueAsString(removedCat);
+        ResultMatcher checkBody = MockMvcResultMatchers.content().json(returnedAsJson);
+        ResultMatcher checkStatus = MockMvcResultMatchers.status().isOk();
+
+         // Send request - and expect 200 status code and the cat (ID 1) from the DB schema
+         this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+    }
+
+    @Test
+    void testUpdate() throws Exception {
+         // URL, method (PATCH), body JSON
+         RequestBuilder req = MockMvcRequestBuilders
+                                    .patch("/update/1")
+                                    .param("name", "Top Cat")
+                                    .param("evil", "false");
+
+        Cat updatedCat = new Cat(1L, "Top Cat", false, false, 10);
+
+        String returnedAsJson = this.mapper.writeValueAsString(updatedCat);
+        ResultMatcher checkBody = MockMvcResultMatchers.content().json(returnedAsJson);
+        ResultMatcher checkStatus = MockMvcResultMatchers.status().isOk();
+
+        // Send request - and expect 200 status code and the
+        // updated cat (new name and EVIl)
+         this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
     }
 }
